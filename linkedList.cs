00da98a62c3edc2ea54e5d9cdab206e1962946b5
Harpreet Gill <735740@student.tor.loc>
2019-07-17 10:08:23 -0400

    linked_list

---
+++ linkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new LinkedList();
+            a.CreateLinkedList();
+        }
+    }
+    class LinkedList
+    {
+        Node Head, Orange, Tangerine, Tail;
+        public void CreateLinkedList()
+        {
+            Head = new Node();
+            Orange = new Node();
+            Tangerine = new Node();
+            //Tail = new Node();
+            Head.next = Orange;
+            Console.WriteLine("Yeah!!!");
+            Console.Read();
+        }
+    }
+
+    class Node
+    {
+        public string studentName;
+        public Node next;
+        public Node prev;
+
+    }
+}
